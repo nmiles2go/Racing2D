4b891df4a08af32e34c94bdda5c7b6f4a85780ce
nmiles2go <jaiswal11.rishikesh@gmail.com>
2025-04-16 14:39:15 -0400

    Tried adding some keyboard input. Still incomplete.

--- Racing2D/main.cpp
+++ Racing2D/main.cpp
@@ -1,23 +1,38 @@
-#include<iostream>
+﻿#include<iostream>
 #include"SFML/Graphics.hpp"
+#include"SFML/Audio.hpp"
+
 
 
 
 int main()
-{   
+{
     // Creating window for our game
-    sf::RenderWindow window(sf::VideoMode({1080, 720}), "2D TWO PLAYER RACER!");
+    sf::RenderWindow window(sf::VideoMode({ 1080, 720 }), "2D TWO PLAYER RACER!");
     window.setFramerateLimit(60);
 
     // Importing 2D Textures for the car and Background
-    sf::Texture T_background, T_car, t3;
+    sf::Texture T_Background, T_Car, t3;
+    sf::RectangleShape Rect({ 2.0f,2.0f });
 
-    T_background.loadFromFile("images/background.png");
-    T_car.loadFromFile("images/car.png");
-    T_background.setSmooth(true);
-    T_car.setSmooth(true);
+    // Importing textures and converting them to sprites
+    T_Background.loadFromFile("images/background.png");
+    T_Car.loadFromFile("images/car.png");
+    T_Background.setSmooth(false);
+    T_Car.setSmooth(false);
 
-    sf::Sprite S_Background = sf::Sprite(T_background, sf::RectangleShape({2.0f, 30.0f})); //TODO figure out the correct methods
+    sf::Sprite S_Background = sf::Sprite(T_Background);
+    S_Background.scale({ 1.5f,1.5f });
+
+    sf::Sprite S_Car = sf::Sprite(T_Car);
+    S_Car.setOrigin({ 22.0f, 22.0f });
+    S_Car.setPosition({200.0f,200.0f});
+    S_Car.scale({ 0.95f, 0.95f });
+    
+    // Car movement variables
+    float speed = 0.0f;
+    float accelaration = 0.3f;
+    
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -26,8 +41,35 @@ int main()
                 window.close();
         }
 
+        // Getting input from keyboard
+
+        //////////////////////////////////////////////////////////
+
+
+        //   ██╗    ██╗ █████╗ ███████╗██████╗
+        //   ██║    ██║██╔══██╗██╔════╝██╔══██╗
+        //   ██║ █╗ ██║███████║███████╗██║  ██║     for car movement.
+        //   ██║███╗██║██╔══██║╚════██║██║  ██║
+        //   ╚███╔███╔╝██║  ██║███████║██████╔╝
+        //    ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚═════╝
+
+        // When key pressed (WASD), boolean values change to true
+
+        bool up    = 0;
+        bool down  = 0;
+        bool left  = 0;
+        bool right = 0;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+            
+            
+            S_Car.setPosition({ 0.0f,0.0f });
+        }
+        
+
         window.clear(sf::Color(158,123,123,0));
-       
+        window.draw(S_Background);
+        window.draw(S_Car);
         window.display();
     }
 }
