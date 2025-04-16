3fa5c9cfe1ba0af53e72e12548f2f6ab8450c5b5
nmiles2go <jaiswal11.rishikesh@gmail.com>
2025-04-16 10:32:34 -0400

    Setting up SMFL with Visual Studio

--- Racing2D/main.cpp
+++ Racing2D/main.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include"SFML/Graphics.hpp"
+
 
 int main()
 {
-	std::cout << "Hello World!";
-	return 0;
+    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
+	
+
