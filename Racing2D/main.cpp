d194d4fedc3cba680ad40e536f050c1167653bb1
nmiles2go <jaiswal11.rishikesh@gmail.com>
2025-04-16 11:07:35 -0400

    Window creation

--- Racing2D/main.cpp
+++ Racing2D/main.cpp
@@ -2,12 +2,22 @@
 #include"SFML/Graphics.hpp"
 
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
+int main()
+{   
+    // Creating window for our game
+    sf::RenderWindow window(sf::VideoMode({1080, 720}), "2D TWO PLAYER RACER!");
+    window.setFramerateLimit(60);
+
+    // Importing 2D Textures for the car and Background
+    sf::Texture T_background, T_car, t3;
+
+    T_background.loadFromFile("images/background.png");
+    T_car.loadFromFile("images/car.png");
+    T_background.setSmooth(true);
+    T_car.setSmooth(true);
+
+    sf::Sprite S_Background = sf::Sprite(T_background, sf::RectangleShape({2.0f, 30.0f})); //TODO figure out the correct methods
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -16,8 +26,8 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color(158,123,123,0));
+       
         window.display();
     }
 }
