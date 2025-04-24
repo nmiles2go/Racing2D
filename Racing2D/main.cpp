08d0b44787b5f395d40c2be4b75aa6a061480b8e
nmiles2go <jaiswal11.rishikesh@gmail.com>
2025-04-24 17:17:39 -0400

    Worked more on the movement. Found a logic bug. Need to understand what the problem is.

--- Racing2D/main.cpp
+++ Racing2D/main.cpp
@@ -26,19 +26,19 @@ int main()
 
     sf::Sprite S_Car = sf::Sprite(T_Car);
     S_Car.setOrigin({ 22.0f, 22.0f });
-    S_Car.setPosition({200.0f,200.0f});
+    S_Car.setPosition({ 200.0f,200.0f });
     S_Car.scale({ 0.95f, 0.95f });
-    
+
     // Car movement variables
-    float speed          = 0.0f;
-    float angle          = 0.0f;
-    float maxSpeed       = 8.0f;
-    float accelaration   = 0.3f;
+    float speed = 0.0f;
+    float angle = 0.0f;
+    float maxSpeed = 2.0f; // changes made for testing revert back to 8.0 later
+    float accelaration = 0.01f;// changes made for testing revert back to 0.3 later
     float deaccelaration = 0.2f;
-    float turnSpeed      = 0.08f;
-    
-    float carPositionX   = S_Car.getPosition().x;
-    float carPositionY   = S_Car.getPosition().y;
+    float turnSpeed = 0.08f;
+
+    float carPositionX = S_Car.getPosition().x;
+    float carPositionY = S_Car.getPosition().y;
 
 
     while (window.isOpen())
@@ -59,88 +59,68 @@ int main()
         //   ╚███╔███╔╝██║  ██║███████║██████╔╝
         //    ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚═════╝
 
-        
+
         // W to Move Forward
         // S to Move Backward
         // A to Turn Left
         // D to Turn Right
-        
+
         // When key pressed (WASD), boolean values change to true
 
-        bool goUp    = 0;
-        bool goDown  = 0;
-        bool turnLeft  = 0;
+        bool goUp = 0;
+        bool goDown = 0;
+        bool turnLeft = 0;
         bool turnRight = 0;
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) goUp = 1;
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) goDown = 1;
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) turnLeft = 1;
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) turnRight = 1;
-        
+
         // std::cout << goLeft << "\t" << goRight << "\t" << goUp << "\t" << goDown << "\n"; // DEBUG_LINE
 
         // Adding Car movement
-        
-        if (goUp && fabs(speed) < maxSpeed)
+
+        // Bug: When the velocity reaches above max speed (how?), then the car speed does not increase in the other direction.
+
+       
+        if (goUp && speed < maxSpeed)
         {
             speed -= accelaration;
+            std::cout << (goUp && speed < maxSpeed) << "\n";
         }
 
-        if (goDown && speed < maxSpeed)
+        if (goDown && speed > -maxSpeed)
         {
             speed += accelaration;
+            std::cout << (goDown && speed > -maxSpeed) << "\n";
         }
 
 
 
-        if (!goUp && !goDown && fabs(speed) > 0) // deaccelation not working
-        {
-            if (!goUp) 
-            {
-
-                speed += deaccelaration;
-
-                if (speed > 0.0f)
-                {
-                    speed = 0.0f;
-                }
-            }
-
-            else if (!goDown) 
-            {
-
-                speed -= deaccelaration;
-
-                if (speed < 0.0f)
-                {
-                    speed = 0.0f;
-                }
-            }
-
-        }
-
-        
-        
-        
         
 
+
+
+
+
+
+
+
+
+            carPositionY += speed;
+            S_Car.setPosition({carPositionX,carPositionY});
+            //S_Background.setPosition({ -carPositionX, -carPositionY }); // Linking "camera" background speed with speed of car!
+                                                                                          // BAD I THINK?
+
+
+            window.clear(sf::Color(158, 123, 123, 0));
+            window.draw(S_Background);
+            window.draw(S_Car);
+
+
+            window.display();
         
-
-        
-
-        carPositionY +=  speed;
-        std::cout << speed << "\t\n";
-        S_Car.setPosition({carPositionX,carPositionY});
-        //S_Background.setPosition({ -carPositionX, -carPositionY }); // Linking "camera" background speed with speed of car!
-                                                                                      // BAD I THINK?
-
-        
-        window.clear(sf::Color(158,123,123,0));
-        window.draw(S_Background);
-        window.draw(S_Car);
-
-
-        window.display();
     }
 }
 	
